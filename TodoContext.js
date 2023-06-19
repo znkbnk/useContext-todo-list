2ffa4ac923679f50294f88987b2fcc3c088e5862
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-19 19:44:15 +0100

    Create TodoContext.js

---
+++ TodoContext.js
@@ -0,0 +1,37 @@
+//TodoContext.js
+import React, { createContext, useState } from 'react';
+
+export const TodoContext = createContext();
+
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <TodoContext.Provider value={{ todos, addTodo, toggleTodo, deleteTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
